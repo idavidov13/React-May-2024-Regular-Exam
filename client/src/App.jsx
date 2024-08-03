0f4504de3591b3a9f7356b4f8dc52672951c0f56
Ivan Davidov <ivandavidov13@yahoo.com>
2024-08-03 19:28:04 +0300

    adding authState

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 import NavBar from "./components/nav/Nav";
 import Home from "./components/home/Home";
@@ -9,26 +10,41 @@ import Login from "./components/login/Login";
 import NotFound from "./components/notFound/NotFound";
 import AddTrade from "./components/addTrade/AddTrade";
 import TradeDetails from "./components/tradeDetails/TradeDetails";
+import { AuthContext } from "./context/authContext";
 
 function App() {
+  const [authState, setAuthState] = useState({});
+
+  const changeAuthState = (state) => setAuthState(state);
+
+  const contextData = {
+    userId: authState._id,
+    email: authState.email,
+    accessToken: authState.accessToken,
+    isAuthenticated: !!authState.email,
+    changeAuthState,
+  };
+
   return (
-    <>
-      <div id="box">
-        <NavBar />
-        <main id="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/trades" element={<AllTrades />} />
-            <Route path="/trades/:id" element={<TradeDetails />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/add-trade" element={<AddTrade />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </>
+    <AuthContext.Provider value={contextData}>
+      <>
+        <div id="box">
+          <NavBar />
+          <main id="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/trades" element={<AllTrades />} />
+              <Route path="/trades/:id" element={<TradeDetails />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/add-trade" element={<AddTrade />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </>
+    </AuthContext.Provider>
   );
 }
 
