9601700d321056c8aa6d1e124047899ddfd0633f
Ivan Davidov <ivandavidov13@yahoo.com>
2024-08-03 13:28:45 +0300

    useForm hook created

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,17 @@
+export function useForm(initiaalValue, submitCallback) {
+  const [values, setValues] = useState(initiaalValue);
+
+  const changeHandler = (e) => {
+    setValues((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    submitCallback(values);
+  };
+
+  return { values, changeHandler, submitHandler };
+}
