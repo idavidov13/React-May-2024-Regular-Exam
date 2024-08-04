a9960809f6d7b61e741b5043d0975b46e81d65e3
Ivan Davidov <ivandavidov13@yahoo.com>
2024-08-04 13:51:20 +0300

    added delete/update trades

--- client/src/api/trades-api.js
+++ client/src/api/trades-api.js
@@ -16,3 +16,13 @@ export const createTrade = async (tradeData) => {
   const result = await request.post(BASE_URL, tradeData);
   return result;
 };
+
+export const deleteTrade = async (tradeId) => {
+  const result = await request.del(`${BASE_URL}/${tradeId}`);
+  return result;
+};
+
+export const updateTrade = async (tradeId, tradeData) => {
+  const result = await request.put(`${BASE_URL}/${tradeId}`, tradeData);
+  return result;
+};
