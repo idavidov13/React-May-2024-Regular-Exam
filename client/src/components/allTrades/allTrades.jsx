899e622b25e1493106deec91565724d0cb490bee
Ivan Davidov <ivan.davidov@primeholding.com>
2024-08-06 14:32:29 +0300

    adding heading

--- client/src/components/allTrades/allTrades.jsx
+++ client/src/components/allTrades/allTrades.jsx
@@ -5,15 +5,19 @@ import { useGetAllTrades } from "../../hooks/useTrades";
 const AllTrades = () => {
   const [trades, setTrades] = useGetAllTrades();
   return (
-    <div className="all-posted-trades">
-      {trades.length > 0 ? (
-        trades.map((trade) => <TradeItem key={trade._id} {...trade} />)
-      ) : (
-        <div className="no-trades-container">
-          <h3>There are no logged trades yet!</h3>
-          <p>Be the first and go get it!</p>
-        </div>
-      )}
+    <div>
+      <h1 className="all-trades-heading">All Posted Trades</h1>
+
+      <div className="all-posted-trades">
+        {trades.length > 0 ? (
+          trades.map((trade) => <TradeItem key={trade._id} {...trade} />)
+        ) : (
+          <div className="no-trades-container">
+            <h3>There are no logged trades yet!</h3>
+            <p>Be the first and go get it!</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
