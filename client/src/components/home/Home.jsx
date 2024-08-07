5a381c21ff70c8180f1bce03eba197df6702cf4d
Ivan Davidov <ivan.davidov@primeholding.com>
2024-08-07 10:07:29 +0300

    adding last 3 trades on the home screen

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,40 +1,73 @@
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../../context/authContext";
+import { useGetAllTrades } from "../../hooks/useTrades";
 
 export default function HomePage() {
   const { isAuthenticated, email } = useAuthContext();
+  const [trades, setTrades] = useGetAllTrades();
 
   return (
     <div className="home-container">
-      <header className="home-header">
-        <h1>Welcome to TradeVault</h1>
-        <p>Your one-stop solution for tracking and managing all your trades.</p>
-      </header>
       <section className="home-content">
-        {isAuthenticated ? (
-          <div className="home-section">
-            <h2>Hello, {email}</h2>
-            <p>Participate in our community by adding new trade</p>
-            <div className="home-links">
-              <Link to="/add-trade" className="home-link">
-                Add Trade
-              </Link>
-            </div>
-          </div>
-        ) : (
-          <div className="home-section">
-            <h2>Get Started</h2>
-            <p>Join us today and start tracking your trades with ease.</p>
-            <div className="home-links">
-              <Link to="/login" className="home-link">
-                Login
-              </Link>
-              <Link to="/register" className="home-link register-link">
-                Register
-              </Link>
-            </div>
-          </div>
-        )}
+        <div className="last-data home-section">
+          {trades.length > 0 ? (
+            trades
+              .reverse()
+              .slice(0, 3)
+              .map((trade) => (
+                <div key={trade._id} className="last-item">
+                  <p>
+                    <strong>Ticker:</strong> {trade.ticker}
+                  </p>
+                  <p>
+                    <strong>Date:</strong>{" "}
+                    {new Date(trade.date).toLocaleDateString()}
+                  </p>
+                  <p>
+                    <strong>Entry Price:</strong> ${trade.entryPrice}
+                  </p>
+                  <div className="divider"></div>
+                </div>
+              ))
+          ) : (
+            <p>No trades available.</p>
+          )}
+          <Link to="/trades" className="all-trades-link">
+            All Trades
+          </Link>
+        </div>
+        <div className="home-section">
+          <header className="home-header">
+            <h1>Welcome to TradeVault</h1>
+            <p>
+              Your one-stop solution for tracking and managing all your trades.
+            </p>
+          </header>
+          {isAuthenticated ? (
+            <>
+              <h2>Hello, {email}</h2>
+              <p>Participate in our community by adding new trade</p>
+              <div className="home-links">
+                <Link to="/add-trade" className="home-link">
+                  Add Trade
+                </Link>
+              </div>
+            </>
+          ) : (
+            <>
+              <h2>Get Started</h2>
+              <p>Join us today and start tracking your trades with ease.</p>
+              <div className="home-links">
+                <Link to="/login" className="home-link">
+                  Login
+                </Link>
+                <Link to="/register" className="home-link register-link">
+                  Register
+                </Link>
+              </div>
+            </>
+          )}
+        </div>
       </section>
     </div>
   );
